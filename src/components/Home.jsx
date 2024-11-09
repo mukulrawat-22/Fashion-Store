9e90e80643a0f3d5d6323e72b4bce9b62116367e
Mukul Rawat <mukulrawat7017@gmail.com>
2024-11-09 21:36:42 +0530

    ui/ux done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Nav from "./Nav";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../utils/Context";
 import Loading from "./Loading";
+import axios from "../utils/axios"
 
 const Home = () => {
   const [products] = useContext(ProductContext);
-  console.log(products);
+
+  const { search } = useLocation();
+  const category = decodeURIComponent(search.split("=")[1]);
+
+   const[filteredproducts, setfilteredproducts]= useState(null)
+
+  const getprouductscategory = async () => {
+    try {
+        const {data} = await axios.get(`/products/category/${category}`);
+      setfilteredproducts(data);
+    } catch (error) {
+        console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    if(!filteredproducts) setfilteredproducts(products);
+    if(category !="undefined")getprouductscategory();
+  },[category,products]);
+
+console.log(filteredproducts);
+
   return products ? (
     <>
       <Nav />
       <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-        {products.map((p, i) => (
-          <Link key={p.id}
+        {filteredproducts&& filteredproducts.map((p, i) => (
+          <Link
+            key={p.id}
             to={`/details/${p.id}`}
             className="mr-3 mb-3 card p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center"
           >
             <div
               className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
               style={{
-                backgroundImage:
-                  `url(${p.image})`,
+                backgroundImage: `url(${p.image})`,
               }}
             ></div>
-            <h1 className="hover:text-blue-300">
-                {p.title}
-            </h1>
+            <h1 className="hover:text-blue-300">{p.title}</h1>
           </Link>
         ))}
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,44 @@
 import React from "react";
+import { useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [products] = useContext(ProductContext);
+
+  let distinct_category =
+    products && products.reduce((acc, cv) => [...acc, cv.category], []);
+  distinct_category = [...new Set(distinct_category)];
+
+  const color = () => {
+    return `rgba(${(Math.random() * 255).toFixed()},${(
+      Math.random() * 255
+    ).toFixed()},${(Math.random() * 255).toFixed()},0.4)`;
+  };
+ 
+
   return (
     <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
       <a
-        className="py-3 px-5 border rounded border-blue-200 text-blue-300"
+        className="py-3 px-5 border rounded border-blue-200 mb-3 text-blue-300"
         href="/create"
       >
         Add New Product
       </a>
       <hr className=" my - 3 w-[80%]" />
       <h1 className="text-2xl mb-3 w-[80%]">Category Filter</h1>
-      <ul className=" w-[80%]">
-        <li className="flex items-center  mb-3">
-          <span className="rounded-full  mr-2 w-[15px] h-[15px] bg-blue-200 "></span>
-          Cat 1
-        </li>
-        <li className="flex items-center  mb-3">
-          <span className="rounded-full  mr-2 w-[15px]  h-[15px] bg-red-200 "></span>
-          Cat 1
-        </li>
-        <li className="flex items-center  mb-3">
-          <span className="rounded-full  mr-2 w-[15px]  h-[15px] bg-green-200 "></span>
-          Cat 1
-        </li>
-      </ul>
+      <div className=" w-[80%]">
+        {distinct_category.map((c, i) => (
+          <Link
+            key={i}
+            to={`/?category=${c}`}
+            className="flex items-center  mb-3"
+          >
+            <span style={{backgroundColor: color()}} className="rounded-full  mr-2 w-[15px] h-[15px] "></span>
+            {c}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 };
